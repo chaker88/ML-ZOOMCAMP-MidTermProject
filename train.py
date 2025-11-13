9a2109de9a56e42fd0c283625f1d2250007ae20a
Chaker Saidi <152811430+chaker88@users.noreply.github.com>
2025-11-13 18:33:59 +0000

    Add the train file and update the notebook

---
+++ train.py
@@ -0,0 +1,18 @@
+import pickle
+
+import pandas as pd
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score, accuracy_score, f1_score
+from sklearn.pipeline import make_pipeline
+import xgboost as xgb
+
+
+def load_data():
+    ...
+def train_model():
+    ...
+def save_model():
+    ...
+
+print("Done")
